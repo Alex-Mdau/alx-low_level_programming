35d597b522228cc704a288a310e9f3c86849a015
Alex Okumu <alexokumu006@gmail.com>
2022-09-12 08:15:51 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - If  c is the input then show 1
+ * Any other case, show 0
+ *
+ * @c: The character is ASCII code
+ *
+ * Return: 1 for letters, otherewise Return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <=122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
