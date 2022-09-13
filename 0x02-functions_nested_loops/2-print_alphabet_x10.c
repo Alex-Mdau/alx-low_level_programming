0fbabcf8da38d11ba0576ae3472b36d0e2a04965
Alex Okumu <alexokumu006@gmail.com>
2022-09-12 21:44:28 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,23 @@
 /**
  * print_alphabet_x10 - print alphabet 10 times
  *
- * Return: Always 0
+ * Return: void
  */
+
 void print_alphabet_x10(void)
 {
-	int i, a;
+	char c;
+	int i = 0;
 
-	for (i = 0; i<= a; i++)
+	while (i < 10)
 	{
-		for (a = 97; a<= 122; a++)
+		c = 'a';
+		while (c <= 'z')
 		{
-			_putchar(a);
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
