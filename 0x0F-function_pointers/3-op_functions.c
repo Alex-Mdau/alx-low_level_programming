43843f8075ee2bceec634b00b6cc31d2a2ef3f37
Alex Okumu <alexokumu006@gmail.com>
2022-10-13 03:07:46 -0700

    Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,23 +1,10 @@
 #include "3-calc.h"
 
 /**
- * op_mod - this calculates mod of two numbers
- * @a: number 1
- * @b: number 2
- * Return: Result of mod of 1, 2
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-		return (-1);
-	return (a % b);
-}
-
-/**
- * op_add - this adds two numbers
- * @a: number 1
- * @b: number 2
- * Return: Result of addition
+ * op_add - add two numbers 
+ * @a: first number
+ * @b: second number
+ * Return: result of addition
  */
 int op_add(int a, int b)
 {
@@ -25,10 +12,32 @@ int op_add(int a, int b)
 }
 
 /**
- * op_div - this divides two numbers
- * @a: number 1
- * @b: number 2
- * Return: Result of division
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of division
  */
 int op_div(int a, int b)
 {
@@ -38,23 +47,14 @@ int op_div(int a, int b)
 }
 
 /**
- * op_sub - this subtracts two numbers
- * @a: number 1
- * @b: number 2
- * Return: Result of subtraction
+ * op_mod - mods two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of mod of a, b
  */
-int op_sub(int a, int b)
+int op_mod(int a, int b)
 {
-	return (a - b);
-}
-
-/**
- * op_mul - this multiplies two numbers
- * @a: number 1
- * @b: number 2
- * Return: Result of multiplication
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
+	if (b == 0)
+		return (-1);
+	return (a % b);
 }
